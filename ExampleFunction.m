fc61ae3227ef084b122f847af8945ff206b8b2c1
Mischa Knabenhans <mischa.knabenhans@uzh.ch>
2019-03-31 20:22:59 +0200

    Added new features to ExampleFunction.m

--- ExampleFunction.m
+++ ExampleFunction.m
@@ -1,28 +1,32 @@
-function [] = ExampleFunction()
+function [myPCE, PCECoeffs, PCEIndices] = ExampleFunction()
 %EXAMPLEFUNCTION (by Mischa Knabenhans) showcases how and in which order
 %one can work with the functions provided in this directory.
 
-ParMatrix = ...
-    [0.03   0.06;   % DES + upper bound by CLASS with YHe given by BBN
-     0.1    0.9;    % DES
-     0.7    1.3;    % KiDS + GAMA
-     0.55   0.8;   % DES
-     -1.3  -0.7;    % [4] Fig. 8, middle left panel, 99.7% CL (red) 
-     0.5    1.0];
+% Get the parameter space of interest
+Large6DParSpace
 
-ParNames = {'Omega_b' 'Omega_m' 'n_s' 'h' 'w_0' 'sigma_8'};
-
-% X_LHS = SamplePraameterSpace(ParMatrix, ParNames, 100, 50, 'Example_N50.dat');
-
+% Compute a UQ input model
 cosmicInput = CreateUQInput(ParMatrix, ParNames);
 
-ED = 'path/to/ED/mat/file'  % path to the ED .mat file
-klim = 0.55                 % upper k limit in percent of maximal k
-prec = 1.0 - 10^(-5)        % PCE precision parameter ("a")
-p = 15                      % order of PCE polynomials
-q = 0.5                     % q-norm for PCE truncation
-mi = 2                      % maximum interaction parameter
-CreatePCE(ED, klim, prec, p, q, mi)
+% Load the experimental design data
+EDfile = strcat('/Users/mischaknabenhans/Desktop/Research/',...
+                'PhD_Projects/Prj1_DMEmulator/EuclidEmulator1/',...
+                '09_BIGGERSPACE/MediumParSpace_capOmega/ED_MS6D_N50.mat');
+ED = load(EDfile);
+
+% Pre-process the ED (if necessary)
+trim = 10;
+[k, ppED] = preprocessEmulatorED(ED, trim);
+
+% Define hyperparameters for surrogate model
+hyperparameters.MaxPolyDeg = 15;
+hyperparameters.pcaVariance = 0.99999;
+hyperparameters.qNorm = 0.5;
+hyperparameters.MaximalInteraction = 2;
+
+% Compute polynomial chaos expansion of ED
+[myPCE, PCECoeffs, PCEIndices] = CreatePCE(ppED, hyperparameters, cosmicInput);
+
 
 end
 

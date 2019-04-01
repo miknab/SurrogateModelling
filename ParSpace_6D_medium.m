88e09b41bdcce95f33f230743a1c64894db72f4c
Mischa Knabenhans <mischa.knabenhans@uzh.ch>
2019-04-02 01:09:33 +0200

    Initial commit of ParSpace_6D_medium.m and renaming of other ParSpace files

---
+++ ParSpace_6D_medium.m
@@ -0,0 +1,14 @@
+%% Medium 6D cosmological parameter space
+
+ParMatrix = [0.035  0.055;   % (capital) Omega_b
+             0.1    0.5;    % (capital) Omega_m 
+             0.85   1.1;    % n_s               
+             0.6    0.75;   % h                 
+            -1.5   -0.5;    % w_0               
+             0.7    0.9];   % sigma_8           
+
+ParNames = {'Omega_b' 'Omega_m' 'n_s' 'h' 'w_0' 'sigma_8'};
+
+% REMARK 
+% =========
+% Parameter ranges are chosen freely
